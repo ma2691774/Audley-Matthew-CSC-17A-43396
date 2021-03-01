5438187ecc3cbfbcb2505d71072c34de0ad8d1ae
ma2691774 <79291547+ma2691774@users.noreply.github.com>
2021-02-28 22:00:38 -0600

    Signed off by: maudley@students.rccd.edu

---
+++ Hmwk/IntroLab/HelloWorld.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   HelloWorld.cpp
+ * Author: Matthew Audley
+ * Created on February 28, 2021, 07:37 PM
+ * Purpose:  first Assignment-HelloWorld project
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Hello World!\n";
+
+    //Exit the Program - Cleanup
+    return 0;
+
+}
